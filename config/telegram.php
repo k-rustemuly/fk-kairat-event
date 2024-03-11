15ea0d12b9e642960e7d386f876e9d6e18b899a1
Kuanysh <developerandroidkz@gmail.com>
2024-03-12 01:25:07 +0500

    db config added

--- config/telegram.php
+++ config/telegram.php
@@ -3,4 +3,10 @@
 return [
     'bot_api_key' => env('TELEGRAM_API_KEY', ''),
     'bot_username' => env('TELEGRAM_USERNAME', ''),
+    'mysql'        => [
+        'host'     => env('DB_HOST', '127.0.0.1'),
+        'user'     => env('DB_USERNAME', 'forge'),
+        'password' => env('DB_PASSWORD', ''),
+        'database' => env('DB_DATABASE', 'forge'),
+    ],
 ];
