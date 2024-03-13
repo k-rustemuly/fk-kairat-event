60230ad8a3fecd140fbe16f25c0bd12755762686
Kuanysh <developerandroidkz@gmail.com>
2024-03-14 03:05:10 +0500

    test new memvber

---
+++ app/Services/Commands/Group/NewchatmembersCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of the PHP Telegram Bot example-bot package.
+ * https://github.com/php-telegram-bot/example-bot/
+ *
+ * (c) PHP Telegram Bot Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Generic message command
+ *
+ * Gets executed when any type of message is sent.
+ *
+ * In this conversation-related context, we must ensure that active conversations get executed correctly.
+ */
+
+namespace App\Services\TelegramBots\InfoBot\Commands\Group;
+
+use Longman\TelegramBot\Commands\SystemCommand;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Exception\TelegramException;
+
+class NewchatmembersCommand extends SystemCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'newchatmembers';
+
+    /**
+     * @var string
+     */
+    protected $description = 'New Chat Members';
+
+    /**
+     * @var string
+     */
+    protected $version = '1.3.0';
+
+    /**
+     * Main command execution
+     *
+     * @return ServerResponse
+     * @throws TelegramException
+     */
+    public function execute(): ServerResponse
+    {
+        $message = $this->getMessage();
+        $members = $message->getNewChatMembers();
+
+        $member_names = [];
+        foreach ($members as $member) {
+            $member_names[] = $member->tryMention();
+        }
+
+        return $this->replyToChat('Hi ' . implode(', ', $member_names) . '!');
+    }
+}
