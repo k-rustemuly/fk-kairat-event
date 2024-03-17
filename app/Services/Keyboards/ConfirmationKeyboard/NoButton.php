0f4f8f69570fef1bacf543c5437eac962854a4c4
Kuanysh <developerandroidkz@gmail.com>
2024-03-17 21:47:56 +0500

    fix bug

--- app/Services/Keyboards/ConfirmationKeyboard/NoButton.php
+++ app/Services/Keyboards/ConfirmationKeyboard/NoButton.php
@@ -2,11 +2,13 @@
 
 namespace App\Services\Keyboards\ConfirmationKeyboard;
 
+use App\Models\Participant;
 use App\Services\Entities\TelegramButton;
 use Longman\TelegramBot\Conversation;
 use Longman\TelegramBot\Entities\CallbackQuery;
 use Longman\TelegramBot\Entities\Keyboard;
 use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Request;
 
 class NoButton extends TelegramButton
 {
@@ -22,6 +24,10 @@ class NoButton extends TelegramButton
         $message = $query->getMessage();
         $chat    = $message->getChat();
         $chat_id = $chat->getId();
+        if(Participant::where('telegram_id', $chat_id)->first()) {
+            $data['text'] = __('panel.telegram.already_exists');
+            return Request::sendMessage($data);
+        }
         $conversation = new Conversation($chat_id, $chat_id, 'start');
 
         $notes = &$conversation->notes;
