c7c44ee1d4ff6f97dfbf6457cfeda2ca6634f9aa
Kuanysh <developerandroidkz@gmail.com>
2024-03-20 03:32:53 +0500

    dash

--- app/MoonShine/Pages/Dashboard.php
+++ app/MoonShine/Pages/Dashboard.php
@@ -6,6 +6,10 @@ namespace App\MoonShine\Pages;
 
 use App\Models\Conversation;
 use App\Models\Participant;
+use App\Models\Question;
+use MoonShine\ActionButtons\ActionButton;
+use MoonShine\Components\FormBuilder;
+use MoonShine\Decorations\Heading;
 use MoonShine\Metrics\DonutChartMetric;
 use MoonShine\MoonShineAuth;
 use MoonShine\Pages\Page;
@@ -34,8 +38,22 @@ class Dashboard extends Page
     public function components(): array
 	{
         if($this->isUser) {
+            if($question = Question::whereNotNull('start')->whereNull('end')->first()) {
+                $variants = $question->variants;
+                return [
+                    FormBuilder::make()
+                        ->fields([
+                            Heading::make($question->title),
+                        ])
+                        ->buttons($variants->map(function($variant){
+                            return ActionButton::make($variant->title)
+                                ->primary();
+                        })->toArray())
+                ];
+            }
             return [];
         }
+
 		return [
             DonutChartMetric::make(__('panel.messages.participants'))
                 ->values([
